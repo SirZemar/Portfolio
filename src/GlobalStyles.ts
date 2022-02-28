68418f847dcfcb3b7043506e7f186913520d62bc
José Marinho <eduardo.u.marinho@gmail.com>
2022-02-28 00:49:19 +0000

    Added: Global variables and styles

--- src/GlobalStyles.ts
+++ src/GlobalStyles.ts
@@ -1,5 +1,10 @@
 import { createGlobalStyle } from "styled-components";
 
+
+export const stylesVariables = {
+    menuWidth: '7rem'
+}
+
 export const GlobalStyle = createGlobalStyle`
     :root {
     --largeWidth: 1280px;
@@ -15,6 +20,23 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         background-color: var(--base);
+        padding-left: ${stylesVariables.menuWidth} ;
     }
 
-` 
+
+    ul {
+        list-style: none;
+        padding: 0;
+        margin: 0;
+    }
+
+    .page_container {
+        width: calc(100vw - ${stylesVariables.menuWidth});
+        height: 100vh;
+    }
+
+    .nav_list {
+        text-decoration: none;   
+    }
+
+`
