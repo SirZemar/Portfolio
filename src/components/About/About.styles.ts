d0e5ebcbaad087931bb64e7b95b1438d0dc98d59
José Marinho <eduardo.u.marinho@gmail.com>
2022-04-09 11:01:34 +0100

    add about grid layout area

--- src/components/About/About.styles.ts
+++ src/components/About/About.styles.ts
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+    display: grid;
+    grid-template: 
+        "e b" 45%
+        "a b" 45%
+        "c c"/ 55% ;
     width: 100%;
     height: 100%;
 `
---
+++ src/components/About/Portrait/Portrait.styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    grid-area: b;
+    justify-self: center;
+
+    .size {
+        height: 100% ;
+    }
+`
