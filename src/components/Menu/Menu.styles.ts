7e49306a24f743386efa01202a4a4d8bbcf464d1
José Marinho <eduardo.u.marinho@gmail.com>
2022-04-21 21:34:56 +0100

    refactor: change background color

--- src/components/Menu/Menu.styles.ts
+++ src/components/Menu/Menu.styles.ts
@@ -6,5 +6,7 @@ export const Wrapper = styled.div`
     min-width: 7rem;
     display: grid;
     grid-template-rows: repeat(3, 1fr);
+    /* background-color: #16161a; */
+    background-color: #656565 ;
 `
 
