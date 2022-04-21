5aff1513d8c1eea95bb62f00afda01c9102f5fd7
José Marinho <eduardo.u.marinho@gmail.com>
2022-04-21 22:08:40 +0100

    add Portfolio layout

--- src/components/Portfolio/Portfolio.styles.ts
+++ src/components/Portfolio/Portfolio.styles.ts
@@ -4,5 +4,11 @@ import { LayoutProps } from "../../common/Layout";
 interface Props extends LayoutProps { }
 
 export const Container = styled.div<Props>`
+    background: red;
+    height: 100%;
+    width: 100%;
+`
 
+export const Title = styled.div<Props>`
+    
 `
--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -1,12 +1,19 @@
 import React from "react";
+import Layout, { position, placement } from "../../common/Layout";
 import { pageWrapper as Wrapper } from "../../common/pageWrapper";
-import { Container } from "./Portfolio.styles";
+import { Container, Title } from "./Portfolio.styles";
+
 
 const Portfolio: React.FC = () => (
     <Wrapper>
-        <Container>
-            <h1>Portfolio</h1>
-        </Container>
+        <Layout>
+            <Title position={position.topSlim} placement={placement.center}>
+                <h1>Portfolio</h1>
+            </Title>
+            <Container position={position.centerWide}>
+
+            </Container>
+        </Layout>
     </Wrapper>
 )
 
