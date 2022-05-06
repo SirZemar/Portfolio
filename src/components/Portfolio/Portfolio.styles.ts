400ca5c333c94a099b42331124c4622a5e6a00d4
José Marinho <eduardo.u.marinho@gmail.com>
2022-05-06 19:56:16 +0100

    add projects gallery

--- src/components/Portfolio/Portfolio.styles.ts
+++ src/components/Portfolio/Portfolio.styles.ts
@@ -3,12 +3,25 @@ import { LayoutProps } from "../../common/Layout";
 
 interface Props extends LayoutProps { }
 
+interface Project {
+    backgroundUrl: string
+}
+
+const color = '255, 0, 0';
+
 export const Container = styled.div<Props>`
-    background: red;
-    height: 100%;
-    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 10% );
+    background-color: rgb(${color}, 0);
 `
 
 export const Title = styled.div<Props>`
     
+`
+
+export const Project = styled.div<Props & Project>`
+    background: url(${props => props.backgroundUrl});
+    background-repeat: no-repeat ;
+    background-size: cover;
+    aspect-ratio: 1 / 1;
 `
--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -1,20 +1,24 @@
 import React from "react";
 import Layout, { position, placement } from "../../common/Layout";
 import { pageWrapper as Wrapper } from "../../common/pageWrapper";
-import { Container, Title } from "./Portfolio.styles";
+import { Container, Title, Project } from "./Portfolio.styles";
+
+import { projectsData } from "../../images/mock";
 
 
 const Portfolio: React.FC = () => (
     <Wrapper>
         <Layout>
             <Title position={position.topSlim} placement={placement.center}>
-                <h1>Portfolio</h1>
+                <h1>What I have been doing</h1>
             </Title>
-            <Container position={position.centerWide}>
-
+            <Container position={position.centerWide} placement={placement.withAuto('center', true)}>
+                {projectsData.map((project, index) =>
+                    <Project key={index} backgroundUrl={project.picture}></Project>
+                )}
             </Container>
         </Layout>
-    </Wrapper>
+    </Wrapper >
 )
 
 export default Portfolio;
