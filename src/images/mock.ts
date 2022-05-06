90d5c43aca0e5e1e98e9a82206d8946ea09d909a
José Marinho <eduardo.u.marinho@gmail.com>
2022-05-06 19:56:47 +0100

    add projects mock

---
+++ src/images/mock.ts
@@ -0,0 +1,64 @@
+interface project {
+    name: string,
+    picture: string,
+    technologies: string[]
+}
+
+export const projectsData: project[] = [{
+    name: 'Bedroom Rivals',
+    picture: 'https://picsum.photos/400?random=1',
+    technologies: ['HMTL', 'CSS', 'Javascript']
+}, {
+    name: 'One Cinema',
+    picture: 'https://picsum.photos/400?random=2',
+    technologies: ['React', 'Typescript', 'Styled Components']
+}, {
+    name: 'Bedroom Rivals',
+    picture: 'https://picsum.photos/400?random=3',
+    technologies: ['HMTL', 'CSS', 'Javascript']
+}, {
+    name: 'One Cinema',
+    picture: 'https://picsum.photos/400?random=4',
+    technologies: ['React', 'Typescript', 'Styled Components']
+}, {
+    name: 'Bedroom Rivals',
+    picture: 'https://picsum.photos/400?random=5',
+    technologies: ['HMTL', 'CSS', 'Javascript']
+}, {
+    name: 'One Cinema',
+    picture: 'https://picsum.photos/400?random=6',
+    technologies: ['React', 'Typescript', 'Styled Components']
+}, {
+    name: 'Bedroom Rivals',
+    picture: 'https://picsum.photos/400?random=7',
+    technologies: ['HMTL', 'CSS', 'Javascript']
+}, {
+    name: 'One Cinema',
+    picture: 'https://picsum.photos/400?random=8',
+    technologies: ['React', 'Typescript', 'Styled Components']
+}, {
+    name: 'Bedroom Rivals',
+    picture: 'https://picsum.photos/400?random=9',
+    technologies: ['HMTL', 'CSS', 'Javascript']
+}, {
+    name: 'One Cinema',
+    picture: 'https://picsum.photos/400?random=10',
+    technologies: ['React', 'Typescript', 'Styled Components']
+}, {
+    name: 'Bedroom Rivals',
+    picture: 'https://picsum.photos/400?random=11',
+    technologies: ['HMTL', 'CSS', 'Javascript']
+}, {
+    name: 'One Cinema',
+    picture: 'https://picsum.photos/400?random=12',
+    technologies: ['React', 'Typescript', 'Styled Components']
+}, {
+    name: 'Bedroom Rivals',
+    picture: 'https://picsum.photos/400?random=13',
+    technologies: ['HMTL', 'CSS', 'Javascript']
+}, {
+    name: 'One Cinema',
+    picture: 'https://picsum.photos/400?random=14',
+    technologies: ['React', 'Typescript', 'Styled Components']
+}
+]
