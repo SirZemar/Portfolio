d5597938ea0cbf47898893a75c52aa2ef49e3375
José Marinho <eduardo.u.marinho@gmail.com>
2022-07-14 19:01:08 +0100

    add portrait image

--- src/components/About/Portrait/Portrait.styles.ts
+++ src/components/About/Portrait/Portrait.styles.ts
@@ -2,8 +2,16 @@ import styled from "styled-components";
 
 export const Container = styled.div`
 
-    .size {
+    .portrait-image {
         max-height: 100%;
         max-width: 100%;
-    }
+        filter: contrast(120%) brightness(90%);
+        border-radius: 5px;
+        animation: opacity 0.7s;
+
+        @keyframes opacity {
+            from {opacity: 0}
+            to {opacity: 1}
+        }
+}
 `
--- src/components/About/Portrait/index.tsx
+++ src/components/About/Portrait/index.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import { Container } from "./Portrait.styles";
 // Types
 import { LayoutProps } from "../../../common/Layout";
+// image
+import portraitImg from "../../../images/euport.jpg";
 
 interface Props extends LayoutProps { }
 
 const Portrait: React.FC<Props> = () => {
     return (
         <Container >
-            <img className="size" src="https://via.placeholder.com/500x600.png/09f/fff" />
+            <img className="portrait-image" src={portraitImg} width="450" />
         </Container>
     )
 }
