05f28b681f5671dbe8f0e3baeb8f7ca5b8764b72
SirZemar <eduardo.w.marinho@gmail.com>
2023-06-05 12:24:03 +0100

    social linkedin url update

--- src/components/Menu/Social/index.tsx
+++ src/components/Menu/Social/index.tsx
@@ -4,14 +4,18 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 
 const Social: React.FC = () => (
-    <Container>
-        <a href="https://www.linkedin.com/in/josé-marinho-11a621216/" target="_blank" rel="noreferrer">
-            <LinkedInIcon />
-        </a>
-        <a href="https://github.com/SirZemar" target="_blank" rel="noreferrer" >
-            <GitHubIcon />
-        </a>
-    </Container>
-)
+  <Container>
+    <a
+      href="https://www.linkedin.com/in/eduardo-marinho-11a621216/"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <LinkedInIcon />
+    </a>
+    <a href="https://github.com/SirZemar" target="_blank" rel="noreferrer">
+      <GitHubIcon />
+    </a>
+  </Container>
+);
 
 export default Social;
