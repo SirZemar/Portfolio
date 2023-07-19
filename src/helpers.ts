a45f44c7cd3fa04f3661753777f0bd0f743e0e7c
SirZemar <eduardo.w.marinho@gmail.com>
2023-07-19 12:19:02 +0100

    add function to check success out of probability

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,17 +1,31 @@
 export const shuffle = (array: any[]) => {
-    let currentIndex = array.length, randomIndex;
+  let currentIndex = array.length,
+    randomIndex;
 
-    // While there remain elements to shuffle...
-    while (currentIndex !== 0) {
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
 
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
 
-        // And swap it with the current element.
-        [array[currentIndex], array[randomIndex]] = [
-            array[randomIndex], array[currentIndex]];
-    }
+  return array;
+};
 
-    return array;
-}
+export const successByProbability = (probability: number) => {
+  const random = Math.random() * 100;
+
+  const decimalRandom = Math.floor(random * 1000) / 1000;
+
+  if (probability >= decimalRandom) {
+    return true;
+  } else {
+    return false;
+  }
+};
