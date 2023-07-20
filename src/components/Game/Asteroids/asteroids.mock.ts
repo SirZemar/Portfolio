3fe046125b2ad022164059f0babe8844a4dcef97
SirZemar <eduardo.w.marinho@gmail.com>
2023-07-20 23:23:32 +0100

    add asteroid mock data

---
+++ src/components/Game/Asteroids/asteroids.mock.ts
@@ -0,0 +1,100 @@
+const asteroidsMock = [
+  {
+    id: 1,
+    path: {
+      from: {
+        left: "100%",
+        top: "Auto",
+        bottom: "40%",
+      },
+      to: {
+        left: "0",
+        bottom: "10%",
+        top: "Auto",
+      },
+    },
+    delay: 0,
+  },
+  {
+    id: 2,
+    path: {
+      from: {
+        left: "100%",
+        top: "Auto",
+        bottom: "70%",
+      },
+      to: {
+        left: "0",
+        bottom: "20%",
+        top: "Auto",
+      },
+    },
+    delay: 2,
+  },
+  {
+    id: 3,
+    path: {
+      from: {
+        left: "100%",
+        top: "Auto",
+        bottom: "90%",
+      },
+      to: {
+        left: "0",
+        bottom: "60%",
+        top: "Auto",
+      },
+    },
+    delay: 4,
+  },
+  {
+    id: 4,
+    path: {
+      from: {
+        left: "90%",
+        top: "Auto",
+        bottom: "100%",
+      },
+      to: {
+        left: "0",
+        bottom: "50%",
+        top: "Auto",
+      },
+    },
+    delay: 6,
+  },
+  {
+    id: 5,
+    path: {
+      from: {
+        left: "100%",
+        top: "Auto",
+        bottom: "0",
+      },
+      to: {
+        left: "0",
+        bottom: "100%",
+        top: "Auto",
+      },
+    },
+    delay: 6.2,
+  },
+  {
+    id: 6,
+    path: {
+      from: {
+        left: "100%",
+        top: "27%",
+        bottom: "Auto",
+      },
+      to: {
+        left: "0",
+        bottom: "Auto",
+        top: "60%",
+      },
+    },
+    delay: 6.4,
+  },
+];
+
+export { asteroidsMock };
