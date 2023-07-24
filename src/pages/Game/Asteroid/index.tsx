159b98076aa13f32ca45d63ec325e0db22c73f6d
SirZemar <eduardo.w.marinho@gmail.com>
2023-07-24 17:05:38 +0100

    animation starts when game start state is true

--- src/pages/Game/Asteroid/index.tsx
+++ src/pages/Game/Asteroid/index.tsx
@@ -8,36 +8,44 @@ import { AsteroidMotion } from "./Asteroid.motion.styles";
 import { AsteroidState } from "../AsteroidCluster/AsteroidCluster.helpers";
 interface Props {
   asteroid: AsteroidsModel.Asteroid;
+  gamePlaying: boolean;
 }
 
-const Asteroid: React.FC<Props> = ({ asteroid }) => (
+const asteroidVariants = {
+  initial: (asteroid: AsteroidsModel.Asteroid) => ({
+    backgroundImage: `url(${asteroidType})`,
+    backgroundPosition: `calc(75px * 0) calc(-75px * ${asteroid.rockType})`,
+    left: asteroid.path.from.left,
+    top: asteroid.path.from.top,
+    bottom: asteroid.path.from.bottom,
+    opacity: 0,
+  }),
+  moveAsteroid: (asteroid: AsteroidsModel.Asteroid) => ({
+    left: asteroid.path.to.left,
+    top: asteroid.path.to.top,
+    bottom: asteroid.path.to.bottom,
+    opacity: 1,
+    transition: {
+      duration: asteroid.pathSpeed,
+      ease: "linear",
+      delay: asteroid.delay,
+      opacity: {
+        duration: 0,
+      },
+    },
+  }),
+};
+
+const Asteroid: React.FC<Props> = ({ asteroid, gamePlaying }) => (
   <AsteroidMotion
     rocktype={asteroid.rockType}
     rotationspeed={asteroid.rotationSpeed}
     as={motion.div}
-    initial={{
-      backgroundImage: `url(${asteroidType})`,
-      backgroundPosition: `calc(75px * 0) calc(-75px * ${asteroid.rockType})`,
-      left: asteroid.path.from.left,
-      top: asteroid.path.from.top,
-      bottom: asteroid.path.from.bottom,
-      opacity: 0,
-    }}
+    initial={"initial"}
+    variants={asteroidVariants}
+    custom={asteroid}
     onClick={() => console.log("Asteroid clicked")}
-    animate={{
-      left: asteroid.path.to.left,
-      top: asteroid.path.to.top,
-      bottom: asteroid.path.to.bottom,
-      opacity: 1,
-      transition: {
-        duration: asteroid.pathSpeed,
-        ease: "linear",
-        delay: asteroid.delay,
-        opacity: {
-          duration: 0,
-        },
-      },
-    }}
+    animate={gamePlaying ? "moveAsteroid" : ""}
     key={asteroid.id}
     onAnimationComplete={() => {
       if (asteroid.impactRoute) {
