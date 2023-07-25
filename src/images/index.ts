eb300b96b2f5c1017cf4252c1170481603532af6
SirZemar <eduardo.w.marinho@gmail.com>
2023-07-25 13:33:50 +0100

    add game cursor images

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,4 +1,7 @@
 import asteroidType from "./asteroids/filter5.png";
 import planetImage from "./planet/space-planet.png";
+import logo from "./logo/elogo.svg";
+import aiming from "./crosshair/aiming-50.png";
+import targetLock from "./crosshair/target-lock-50.png";
 
-export { asteroidType, planetImage };
+export { asteroidType, planetImage, logo, aiming, targetLock };
