7ea92d9059826fe034383c76bece9b9d9076fe24
SirZemar <eduardo.w.marinho@gmail.com>
2023-07-26 15:32:55 +0100

    menu move to top of elements

--- src/pages/Menu/Menu.styles.ts
+++ src/pages/Menu/Menu.styles.ts
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    height: 100%;
-    min-width: 7rem;
-    display: grid;
-    grid-template-rows: repeat(3, 1fr);
-    /* background-color: #16161a; */
-    background-color: #656565 ;
-`
+  height: 100%;
+  min-width: 7rem;
+  display: grid;
+  grid-template-rows: repeat(3, 1fr);
+  /* background-color: #16161a; */
+  background-color: #656565;
+  z-index: 100;
+`;
 
