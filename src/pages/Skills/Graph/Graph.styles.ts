96ea2dc233e0fd1e0873c5c10922ed99ac314acd
SirZemar <eduardo.w.marinho@gmail.com>
2023-08-03 17:24:15 +0100

    fix(skills): multiple routing cause excessive delay on progress bar animation

--- src/pages/Skills/Graph/Graph.styles.ts
+++ src/pages/Skills/Graph/Graph.styles.ts
@@ -1,47 +1,41 @@
-import styled, { keyframes } from "styled-components";
+import { motion } from "framer-motion";
+import styled from "styled-components";
 
 interface Props {
-    skill: {
-        name: string,
-        width: string
-    },
-    alternate: boolean,
-    delay: number
+  skill: {
+    name: string;
+    width: string;
+  };
+  alternate: boolean;
 }
 
 const background = {
-    red: `rgb(230, 0, 0)`,
-    pink: `rgb(230, 150, 200)`
-}
+  red: `rgb(230, 0, 0)`,
+  pink: `rgb(230, 150, 200)`,
+};
 
 const shadow = {
-    red: `0 0 8px rgb(255, 0, 0, 0.5)`,
-    pink: `0 0 8px rgb(235, 150, 230, 0.5)`
-}
+  red: `0 0 8px rgb(255, 0, 0, 0.5)`,
+  pink: `0 0 8px rgb(235, 150, 230, 0.5)`,
+};
 
-const barFill = (width: string) => keyframes`
-    from {width: 0}
-    to {width: ${width}}
-`
-
-export const ProgressBar = styled.div<Props>`
-    width: 0;
-    background-color: ${props => props.alternate ? background.red : background.pink};
-    box-shadow: ${props => props.alternate ? shadow.red : shadow.pink};
-    animation: ${props => barFill(props.skill.width)} 600ms ${props => props.delay}s forwards;
-`
+export const ProgressBar = styled(motion.div)<Props>`
+  width: 0;
+  background-color: ${({ alternate }) =>
+    alternate ? background.red : background.pink};
+  box-shadow: ${({ alternate }) => (alternate ? shadow.red : shadow.pink)};
+`;
 
 export const Container = styled.div`
+  .bar-container {
+    display: block;
+    width: 100%;
+    height: 3px;
+    box-shadow: inset 0 0 0 3px grey;
 
-    .bar-container {
-        display: block;
-        width: 100%;
-        height: 3px;
-        box-shadow: inset 0 0 0 3px grey;
-
-        ${ProgressBar} {
-            height: 100%;
-            border-radius: 5px;
-        }
+    ${ProgressBar} {
+      height: 100%;
+      border-radius: 5px;
     }
-`
+  }
+`;
--- src/pages/Skills/Graph/index.tsx
+++ src/pages/Skills/Graph/index.tsx
@@ -2,39 +2,48 @@ import React, { useRef } from "react";
 import { Container, ProgressBar } from "./Graph.styles";
 // Types
 import { LayoutProps } from "../../../common/Layout";
+import { motion } from "framer-motion";
 
-interface Props extends LayoutProps { };
+interface Props extends LayoutProps {}
 
 const skills = [
   { name: "HTML", width: "75%" },
   { name: "CSS", width: "85%" },
-  { name: "JavaScript", width: "75%" },
-  { name: "React", width: "60%" },
+  { name: "JavaScript", width: "85%" },
+  { name: "React", width: "65%" },
   { name: "Angular", width: "70%" },
-  { name: "Firebase", width: "45%" },
+  { name: "Firebase", width: "55%" },
   { name: "AWS", width: "45%" },
   { name: "Node.js", width: "65%" },
 ];
 
 const Graph: React.FC<Props> = () => {
+  const alternate = useRef(false);
+  const delay = useRef(0);
 
-    const alternate = useRef(false);
-    const delay = useRef(0);
-
-    return (
-        <Container >
-            <ul>
-                {skills.map((skill) =>
-                    <li key={skill.name}>
-                        <p>{skill.name}</p>
-                        <div className="bar-container">
-                            <ProgressBar skill={skill} alternate={alternate.current = !alternate.current} delay={delay.current = delay.current + 0.1} />
-                        </div>
-                    </li>
-                )}
-            </ul>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <ul>
+        {skills.map((skill, index) => (
+          <li key={skill.name}>
+            <p>{skill.name}</p>
+            <div className="bar-container">
+              <ProgressBar
+                as={motion.div}
+                skill={skill}
+                alternate={(alternate.current = !alternate.current)}
+                initial={{ width: "0%" }}
+                animate={{
+                  width: skill.width,
+                  transition: { duration: 0.5, delay: index * 0.1 },
+                }}
+              />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </Container>
+  );
+};
 
 export default Graph;
