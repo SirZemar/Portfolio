1f9348e4ba55de8aeb9f52983da8379f7f9f1b14
SirZemar <eduardo.w.marinho@gmail.com>
2023-08-03 11:53:56 +0100

    add explosion image

--- src/images/index.ts
+++ src/images/index.ts
@@ -3,5 +3,13 @@ import planetImage from "./planet/space-planet.png";
 import logo from "./logo/elogo.svg";
 import aiming from "./crosshair/aiming-50.png";
 import targetLock from "./crosshair/target-lock-50.png";
+import asteroidExplosion from "./asteroids/explosion/explosion1.png";
 
-export { asteroidType, planetImage, logo, aiming, targetLock };
+export {
+  asteroidType,
+  planetImage,
+  logo,
+  aiming,
+  targetLock,
+  asteroidExplosion,
+};
